750186cffe5eaab3a96d02d865aaabee937a55a7
“berenpolat” <polatberen24@gmail.com>
2023-08-18 10:42:01 +0300

    .

--- Assets/Scripts/FallingFromShip1.cs
+++ Assets/Scripts/FallingFromShip1.cs
@@ -5,16 +5,15 @@ using DG.Tweening;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class FallingFromShip : MonoBehaviour
+public class FallingFromShip1 : MonoBehaviour
 {
     [SerializeField] private GameObject clouds;
     [SerializeField] private GameObject boomEffect;
     private void Start()
     {
         transform.DOMove(new Vector3(0, 0.779999971F, 0), 1F);
-        clouds.transform.DOMove(new Vector3(-1.74192357F, 3.21699905F, 12.3390713F), 2F);
+        clouds.transform.DOMove(new Vector3(-1.74192357F, 15F, 12.3390713F), 2F);
         boomEffect.transform.DOMove(new Vector3(-0.0636740923f,0.230000004f,0), 1.5f);
-        Destroy(boomEffect,3f);
         StartCoroutine(Movements());
     }
 
