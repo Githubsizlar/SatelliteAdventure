48083fb4f776e2beb20683cc27c272a917e17b02
“berenpolat” <polatberen24@gmail.com>
2023-08-20 21:56:03 +0300

    walking

--- Assets/Scripts/CameraFollowsPlayer.cs
+++ Assets/Scripts/CameraFollowsPlayer.cs
@@ -5,22 +5,19 @@ using UnityEngine;
 
 public class CameraFollowsPlayer : MonoBehaviour
 {
-    public Transform target;        // The character's transform to follow
-    public Vector3 offset = new Vector3(0f, 3f);  // Camera offset from the character in 2D;
-    public float followSpeed = 5f;  // Speed factor for camera movement
+    public Transform target;           // The player's transform to follow
+    public Vector3 offset = new Vector3(0, 0, -10); // Offset from the player's position
+
+    [Range(0, 1)]
+    public float smoothSpeed = 0.125f; // Smoothing factor for camera movement
 
     private void LateUpdate()
     {
         if (target == null)
             return;
 
-        // Calculate the desired camera position by adding the offset to the target's position
-        Vector3 desiredPosition = new Vector3(target.localPosition.x + offset.x, target.localPosition.y + offset.y, transform.localPosition.z);
-
-        // Calculate the new position by moving the camera directly towards the desired position with a constant speed
-        Vector3 newPosition = Vector3.MoveTowards(transform.position, desiredPosition, followSpeed * Time.deltaTime);
-
-        // Set the camera's position directly to the new position
-        transform.position = newPosition;
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.localPosition, desiredPosition, smoothSpeed);
+        transform.position = smoothedPosition;
     }
 }
