fec072fe5ce305e7ee755405230100497dfa5dda
Gizoskos <gizemgultoprak2004@gmail.com>
2023-08-24 21:49:15 +0300

    InventoryUI

---
+++ Assets/Scripts/UI/Inventory_UI.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory_UI : MonoBehaviour
+{
+    public GameObject inventoryPanel;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            ToggleInventory();
+        }
+    }
+    public void ToggleInventory()
+    {
+        if (!inventoryPanel.activeSelf)
+        {
+            inventoryPanel.SetActive(true);
+        }
+        else
+        {
+            inventoryPanel.SetActive(false);
+        }
+    }
+}
