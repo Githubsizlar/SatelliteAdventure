201fd06d638352f7069ad5cdd92ff98cb61e2430
Gizoskos <gizemgultoprak2004@gmail.com>
2023-08-25 20:59:18 +0300

    Button geldi ayol

--- Assets/Scripts/UI/Inventory_UI.cs
+++ Assets/Scripts/UI/Inventory_UI.cs
@@ -5,24 +5,39 @@ using UnityEngine;
 public class Inventory_UI : MonoBehaviour
 {
     public GameObject inventoryPanel;
+    public PlayerMovement player;
+    public List<Slot_UI> slots = new List<Slot_UI>();
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-            ToggleInventory();
-        }
+        if (Input.GetKeyDown(KeyCode.Tab))
+        { ToggleInventory(); }
     }
     public void ToggleInventory()
     {
         if (!inventoryPanel.activeSelf)
-        {
-            inventoryPanel.SetActive(true);
+        { inventoryPanel.SetActive(true);
+            Setup();
         }
         else
         {
             inventoryPanel.SetActive(false);
         }
+
+    }
+    void Setup()
+    {
+        if (slots.Count == player.inventory.slots.Count)
+        {
+            for (int i = 0; i < slots.Count; i++)
+            {
+                if (player.inventory.slots[i].type != CollectableType.NONE)
+                {
+
+                }
+            }
+        }
+ 
     }
 }
